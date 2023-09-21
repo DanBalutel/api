9fe774e73c5ee448da58ab76f1edfd2ef449287a
DanBalutel <danbalutel@gmail.com>
2023-09-21 12:32:14 +0300

    testing

--- server.js
+++ server.js
@@ -9,35 +9,35 @@ const targetUrl = 'http://3.67.38.226:3000/';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Enable CORS headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+// // Enable CORS headers
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
-    // Respond to preflights
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
+//     // Respond to preflights
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
 
-app.use('/', async (req, res) => {
-    try {
-        const forwardUrl = targetUrl + req.path;
-        const response = await axios({
-            method: req.method,
-            url: forwardUrl,
-            data: req.body,
-            headers: req.headers
-        });
+// app.use('/', async (req, res) => {
+//     try {
+//         const forwardUrl = targetUrl + req.path;
+//         const response = await axios({
+//             method: req.method,
+//             url: forwardUrl,
+//             data: req.body,
+//             headers: req.headers
+//         });
 
-        res.status(response.status).send(response.data);
-    } catch (error) {
-        console.error('Error in the proxy:', error.response.statusText);
-        res.status(error.response.status).send(error.response.statusText);
-    }
-});
+//         res.status(response.status).send(response.data);
+//     } catch (error) {
+//         console.error('Error in the proxy:', error.response.statusText);
+//         res.status(error.response.status).send(error.response.statusText);
+//     }
+// });
 
 app.get('/ping', (req, res) => {
 	res.status(200).send("PONG");

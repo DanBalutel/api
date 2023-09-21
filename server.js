87542f0bf26600ce98d5cec37dc467285a9209ad
DanBalutel <danbalutel@gmail.com>
2023-09-21 13:10:48 +0300

    ds

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ const targetUrl = 'http://3.67.38.226:3000/';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// // Enable CORS headers
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', '*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+// Enable CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
-//     // Respond to preflights
-//     if (req.method === 'OPTIONS') {
-//         return res.sendStatus(200);
-//     }
-//     next();
-// });
+    // Respond to preflights
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    next();
+});
+
+app.get('/get', (req, res) => {
+	res.send("PONG");
+});
 
 app.use('/', async (req, res) => {
     try {
@@ -46,11 +50,6 @@ app.use('/', async (req, res) => {
         console.error('Error in the proxy:', error.response ? error.response.statusText : error.message);
         res.status(error.response ? error.response.status : 500).send(error.response ? error.response.statusText : "Internal Proxy Error");
     }
-});
-
-
-app.get('/get', (req, res) => {
-	res.send("PONG");
 });
 
 

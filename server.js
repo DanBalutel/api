2e543f6167c4e9402bf220e63226dbb937a9ea2c
DanBalutel <danbalutel@gmail.com>
2023-09-21 13:05:04 +0300

    removed cors

--- server.js
+++ server.js
@@ -9,18 +9,18 @@ const targetUrl = 'http://3.67.38.226:3000/';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Enable CORS headers
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+// // Enable CORS headers
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
 
-    // Respond to preflights
-    if (req.method === 'OPTIONS') {
-        return res.sendStatus(200);
-    }
-    next();
-});
+//     // Respond to preflights
+//     if (req.method === 'OPTIONS') {
+//         return res.sendStatus(200);
+//     }
+//     next();
+// });
 
 app.use('/', async (req, res) => {
     try {
